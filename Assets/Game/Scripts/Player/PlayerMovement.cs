bb20d9e50c6ec8c7d6078252fc51b4b3ff950a16
Nadinda <nadinda@nadinda.dev>
2025-03-03 23:24:11 +1100

    Week 1 - Remove deltaTime when addForce on Jump

--- Assets/Game/Scripts/Player/PlayerMovement.cs
+++ Assets/Game/Scripts/Player/PlayerMovement.cs
@@ -94,7 +94,7 @@ public class PlayerMovement : MonoBehaviour
         if (_isGrounded)
         {
             Vector3 jumpDirection = Vector3.up;
-            _rigidbody.AddForce(jumpDirection * _jumpForce * Time.deltaTime);
+            _rigidbody.AddForce(jumpDirection * _jumpForce);
         }
     }
 
